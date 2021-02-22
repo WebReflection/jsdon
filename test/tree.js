5b18c7ef5889709586fb54a051a085e2b833ea35
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2021-02-22 11:39:06 +0100

    added asTree(jsdon) tests

--- test/tree.js
+++ test/tree.js
@@ -1,6 +1,9 @@
+const {toJSON} = require('../cjs/to-json.js');
 const {asTree} = require('../cjs/as-tree.js');
 
-const document = asTree([9,10,"html",1,"html",2,"lang","en",1,"head",-1,1,"body",3,"\n    Text\n    ",8,"Comment",-3]);
+const json = [9,10,"html",1,"html",2,"lang","en",1,"head",-1,1,"body",3,"\n    Text\n    ",8,"Comment",-3];
+
+const document = asTree(json);
 const html = document.childNodes[1];
 const body = html.childNodes[1];
 
@@ -9,4 +12,4 @@ console.assert(html.parentNode === document);
 console.assert(body.parentNode === html);
 console.assert(body.childNodes.every(node => node.parentNode === body));
 
-console.log(document);
+console.assert(JSON.stringify(toJSON(document)) === JSON.stringify(json));
